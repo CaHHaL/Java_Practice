96ce3cb67fc815ea2bd5f0ceba12de7c3c61bd00
Cahal Agarwalla <cahalagarwalla333@gmail.com>
2025-02-07 15:41:01 +0530

    some questions

---
+++ Q4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+enum color
+{
+RED,GREEN,YELLOW;    
+}
+class Main{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String input=s.next().toUpperCase();
+        color type=color.valueOf(input);
+        switch(type){
+            case RED:
+                System.out.println("STOP");
+                break;
+            case YELLOW:
+                System.out.println("Wait");
+                break;
+            case GREEN:
+                System.out.println("GO");
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+    }
+}
