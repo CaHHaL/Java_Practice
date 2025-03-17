98d87646cc6ccc7d4fefdfdf7e1f2b5a0be0f4ac
Cahal Agarwalla <cahalagarwalla333@gmail.com>
2025-03-17 14:46:31 +0530

    static nested class

--- static_nestedClass.java
+++ static_nestedClass.java
@@ -80,3 +80,31 @@ class Main{
 //output
 //instance 10
 //static 20
+
+
+
+
+//error code
+//output
+//error code because we cannot make the outer class static 
+static class outer{
+    // int x=10;
+    static int y=20;
+    private int z=30;
+    static class innerClass{
+        void display(){
+            System.out.println("static "+y);
+            System.out.println("private "+z);
+            // System.out.println("instance "+x);
+        }
+    }
+}
+class Main{
+    public static void main(String args[]){
+        
+        outer.innerClass m=new outer.innerClass();
+        m.display();
+    }
+}
+//output
+//error code because we cannot make the outer class static 
